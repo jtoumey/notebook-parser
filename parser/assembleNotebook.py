5a0c64515f06e50b4527bc0c04b542757f5f9a8e
Julian Toumey <julian.toumey@uconn.edu>
2019-08-27 11:37:10 -0400

    Got the notebook assembler to a point at which it is usable. Needs a few more tweaks.

--- parser/assembleNotebook.py
+++ parser/assembleNotebook.py
@@ -7,6 +7,30 @@ import parser
 
 class Notebook:
 
+    preamble = """\\documentclass{article}
+\\usepackage[T1]{fontenc}
+\\usepackage{tgpagella}
+
+\\usepackage[margin=1in]{geometry}
+\\usepackage{amsmath}
+\\usepackage{tabu}
+
+\\usepackage{multicol}
+\\usepackage{booktabs}
+\\usepackage{graphicx}
+
+\\title{Laboratory Notebook \\\\ Computational Thermal Fluids Lab}
+\\author{Julian Toumey\\thanks{julian.toumey@uconn.edu}}
+\\date{Maintained: 05 August 2019--Present}
+
+\\begin{document}
+
+\\maketitle
+
+"""
+    month_dict = {'01': 'JAN', '02': 'FEB', '03': 'MAR', '04': 'APR', '05': 'MAY', '06': 'JUN', '07': 'JUL', '08': 'AUG', '09': 'SEP', '10': 'OCT', '11': 'NOV', '12': 'DEC'}
+    month_header_dict = {'01': 'January', '02': 'February', '03': 'March', '04': 'April', '05': 'May', '06': 'June', '07': 'July', '08': 'August', '09': 'September', '10': 'October', '11': 'November', '12': 'December'}
+
     directory_list = list()
     filtered_years = list()
     filtered_months = list()
@@ -41,21 +65,28 @@ class Notebook:
         print main_nb_path
         main_nb = open(main_nb_path, 'w')
 
-        main_nb.write('\\documentclass{article} \n \\begin{document} \n')
+        # main_nb.write('\\documentclass{article} \n \\begin{document} \n')
+        main_nb.write(self.preamble)
 
         for year in self.filtered_years:
 
-            main_nb.write('\\section{' + year + '}\n')
+            main_nb.write('\\section*{%s}\n\n' % year)
 
             for month in self.filtered_months:
 
-                main_nb.write('\\subsection{%s}\n' % month)
+                month_header = self.month_header_dict[month]
+
+                main_nb.write('\\subsection*{%s}\n\n' % month_header)
 
                 for day in self.filtered_days:
 
                     if '.tex' in day:
                         filepath = year + '/' + month + '/' + day
-                        main_nb.write('\\subsubsection{%s}\n\\input{%s}\n' % (month, filepath))
+                        month_letter = self.month_dict[month]
+                        date_string = day[6:8] +'-' + month_letter
+
+                        
+                        main_nb.write('\\subsubsection*{%s}\n\\input{%s}\n\n' % (date_string, filepath))
 
 
         main_nb.write('\n\\end{document}\n')
